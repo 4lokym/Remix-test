87f3d8ed4c88501db517ec68f269518ecc536aeb
4lokym <mykola.koshilko@gmail.com>
2025-03-16 11:09:30 +0100

    Add aggregator interface

--- contracts/FoundMe.sol
+++ contracts/FoundMe.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.6 <0.9.0;
+pragma solidity >0.6.0 <0.9.0;
+
+
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
 contract FoundMe{
 
@@ -8,5 +11,16 @@ contract FoundMe{
 
     function found() public payable{
         adrressToAmountFounded[msg.sender] += msg.value;
+    }
+
+    function getVersion() public view returns (uint256){
+        AggregatorV3Interface pricefeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return pricefeed.version();
+    }
+
+    function getPrice() public view returns(uint256){
+        AggregatorV3Interface pricefeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 answer,,,) = pricefeed.latestRoundData();
+        return uint(answer);
     }
 }
